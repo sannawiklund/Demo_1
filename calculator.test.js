ae2a32f1809d3d5a7dfa07b9e3605343355b5772
Sanna Wiklund <sanna@webmind.se>
2024-12-20 10:45:39 +0100

    Initial commit

---
+++ calculator.test.js
@@ -0,0 +1,7 @@
+const { add } = require("./calculator");
+
+test("adds two numbers correctly", () => {
+  expect(add(2, 3)).toBe(5);
+  expect(add(-1, 1)).toBe(0);
+  expect(add(0, 0)).toBe(0);
+});
